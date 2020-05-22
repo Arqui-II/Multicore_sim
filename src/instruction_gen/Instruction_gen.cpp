e2ece2af8dd90e5329b5a39786fffd39863454c5
Juan Pablo Brenes C. <juanp1995@gmail.com>
2020-05-22 03:14:44 -0600

    Generate instructions following Poisson distribution

--- src/instruction_gen/Instruction_gen.cpp
+++ src/instruction_gen/Instruction_gen.cpp
@@ -5,21 +5,55 @@
  *      Author: juanp1995
  */
 
-#include "instruction_gen/Instruction_gen.h"
+#include <instruction_gen/Instruction_gen.h>
 
 Instruction_gen::Instruction_gen() {
+	_randGenerator = std::mt19937(time(NULL));
+	_poissonDist = std::poisson_distribution<int>(POISSON_MEAN);
 }
 
 Instruction_gen::~Instruction_gen() {
 }
 
-Instruction_gen::Instruction Instruction_gen::getInst(){
+/**
+ * Generates a random number with a Poisson distribution
+ * to determine the type of instruction.
+ *
+ * @return Integer that represents the type of instruction
+ */
+int Instruction_gen::getInstructionType() {
+	int number = _poissonDist(_randGenerator);
+	if (number >= 0 && number < 8) //READ
+		return 0;
+	else if (number >= 8 && number < 16) //CALC
+		return 1;
+	else if (number >= 16 && number < 24) //WRITE
+		return 2;
+	else
+		return getInstructionType();
+}
+
+int Instruction_gen::getRandomAddress() {
+	std::uniform_int_distribution<int> distr(0, 16);
+	return distr(_randGenerator);
+}
+
+int Instruction_gen::getRandomData() {
+	std::uniform_int_distribution<int> distr(0, 65535);
+	return distr(_randGenerator);
+}
+
+Instruction_gen::Instruction Instruction_gen::getInst(int pChipID, int pCoreID) {
+	int type = getInstructionType();
+	int data = 0;
+	if (type == 2) data = getRandomData();
+
 	Instruction inst;
-	inst.chip = 0;
-	inst.core = 0;
-	inst.op = 2;
-	inst.dest = 15;
-	inst.data = 4;
+	inst.chip = pChipID;
+	inst.core = pCoreID;
+	inst.op = getInstructionType();
+	inst.dest = getRandomAddress();
+	inst.data = data;
 
 	return inst;
 
--- src/instruction_gen/Instruction_gen.h
+++ src/instruction_gen/Instruction_gen.h
@@ -8,10 +8,25 @@
 #ifndef _INSTRUCTION_GEN_H
 #define _INSTRUCTION_GEN_H
 
+#include <random>
+#include <time.h>
+#include <functional>
+
 class Instruction_gen {
 
 private:
-	struct Instruction{
+	const double POISSON_MEAN = 12;
+
+	//std::random_device rd; // uniformly-distributed integer random number generator
+	std::mt19937 _randGenerator; // mt19937: Pseudo-random number generation
+	std::poisson_distribution<int> _poissonDist;
+
+	int getInstructionType();
+	int getRandomAddress();
+	int getRandomData();
+
+public:
+	struct Instruction {
 		int chip;
 		int core;
 		int op;
@@ -19,12 +34,10 @@ private:
 		int data;
 	};
 
-
-public:
 	Instruction_gen();
 	virtual ~Instruction_gen();
 
-	Instruction getInst();
+	Instruction getInst(int pChipID, int pCoreID);
 };
 
 #endif /* _INSTRUCTION_GEN_H_ */
