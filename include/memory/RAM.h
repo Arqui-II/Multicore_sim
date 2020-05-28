6e570ca3babdb68e76054a57ed623f65a692717f
Juan Pablo Brenes C. <juanp1995@gmail.com>
2020-05-28 04:42:26 -0600

    Functions to read and write with mutex

--- include/memory/RAM.h
+++ include/memory/RAM.h
@@ -8,10 +8,31 @@
 #ifndef RAM_H_
 #define RAM_H_
 
+#include <thread>
+#include <mutex>
+#include <Constants.h>
+
 class RAM {
+
+private:
+	struct Block {
+		int owners[cons::NUMBER_OF_CORES];
+		int data;
+	};
+
+	std::mutex _mutex;
+
+	Block _ram[cons::memory::RAM_SIZE];
+
+	void init();
+
 public:
 	RAM();
 	virtual ~RAM();
+
+	int read(int pAddress, int pOwner);
+	void write(int pAddress, int pData, int pOwner);
+
 };
 
 #endif /* RAM_H_ */
--- src/memory/RAM.cpp
+++ src/memory/RAM.cpp
@@ -8,11 +8,44 @@
 #include <memory/RAM.h>
 
 RAM::RAM() {
-	// TODO Auto-generated constructor stub
-
+	this->init();
 }
 
 RAM::~RAM() {
-	// TODO Auto-generated destructor stub
+}
+
+/**
+ * Initializes the RAM with 0s and undefined owners
+ */
+void RAM::init() {
+	for (int i = 0; i < cons::memory::RAM_SIZE; i++) {
+		_ram[i].data = 0;
+		_ram[i].owners[0] = -1;
+		_ram[i].owners[1] = -1;
+	}
+}
+
+int RAM::read(int pAddress, int pOwner) {
+	int value = this->_ram[pAddress].data;
+	std::lock_guard<std::mutex> lock(_mutex);
+	this->_ram[pAddress].owners[pOwner] = pOwner;
+//	if (this->_ram[pAddress].owners[0] == -1 && this->_ram[pAddress].owners[1] == -1)
+//		this->_ram[pAddress].owners[0] = pOwner;
+//
+//	else if (this->_ram[pAddress].owners[0] != -1 && this->_ram[pAddress].owners[0] != pOwner) {
+//		if (this->_ram[pAddress].owners[1] != pOwner) this->_ram[pAddress].owners[1] = pOwner;
+//	}
+//
+//	else if (this->_ram[pAddress].owners[0] == -1 && this->_ram[pAddress].owners[1] != pOwner)
+//		this->_ram[pAddress].owners[0] = pOwner;
+
+	return value;
+}
+
+void RAM::write(int pAddress, int pData, int pOwner) {
+	std::lock_guard<std::mutex> lock(_mutex);
+	this->_ram[pAddress].data = pData;
+	this->_ram[pAddress].owners[0] = pOwner; //Chip who writes is the only owner
+	this->_ram[pAddress].owners[1] = -1; //Delete other owner
 }
 
